92075e96668d318ddf79dff6dd6d7c7021d877c0
kellyl09-create <kellyl09@dow.catholic.edu.au>
2025-10-20 11:57:58 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 basic.forever(function () {
-	
+    music.play(music.stringPlayable("E C A D G - D - ", 120), music.PlaybackMode.UntilDone)
 })
